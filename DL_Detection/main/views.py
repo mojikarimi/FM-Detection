314bf7093cd55908235792aebfc75a6c2a01b548
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-08-05 06:57:35 +0330

    added video_page image_page and webcam_page views

--- DL_Detection/main/views.py
+++ DL_Detection/main/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+def video_page(request):
+    return render(request, 'main/video.html')
+
+
+def image_page(request):
+    return render(request, 'main/image.html')
+
+
+def webcam_page(request):
+    return render(request, 'main/camera.html')
