8bd7a9ed106f83750d6954f137f9cfb798dbfeba
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-08-05 10:21:30 +0330

    create Duc model

--- DL_Detection/DL_Detection/urls.py
+++ DL_Detection/DL_Detection/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path,include
+from django.urls import re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^',include('main.urls'))
+    re_path(r'^', include('main.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- DL_Detection/main/models.py
+++ DL_Detection/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Duc(models.Model):
+    class Meta:
+        verbose_name_plural = 'Documents For FMD'
+
+    title = models.CharField(max_length=150, blank=True)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
