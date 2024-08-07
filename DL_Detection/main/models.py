b7b3d9a73ffff18e73ecff56cf9a1599bb2b99fe
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-08-07 08:33:04 +0330

    added FilesPredicts model

--- DL_Detection/main/models.py
+++ DL_Detection/main/models.py
@@ -13,3 +13,16 @@ class Duc(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class FilesPredicts(models.Model):
+    class Meta:
+        verbose_name_plural = 'Files Prediction'
+
+    original_file = models.CharField(max_length=150, blank=True)
+    original_file_url = models.TextField(blank=True)
+    Prediction_file = models.CharField(max_length=150, blank=True)
+    Prediction_file_url = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.original_file
