518ef2f7f508ee42093573312232a3b756ea644f
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-08-07 08:33:37 +0330

    set the FilesPredicts model

--- DL_Detection/main/admin.py
+++ DL_Detection/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main.models import Duc
+from main.models import Duc,FilesPredicts
 
 # Register your models here.
 admin.site.register(Duc)
+admin.site.register(FilesPredicts)
